902bbdb2419c96f70187c2c1b3e4bea1badf14aa
kenney-g <goadke@g.cofc.edu>
2020-09-11 00:52:01 -0400

    added routing for sessions controller re - logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
     end
   
     post '/login' do 
-  
+      user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password]) 
+          session[:id] = user.id
+          redirect "/users/index"
+        else
+          @error = "I couldn't get that to work. Try something else?" 
+          erb :'/sessions/lgoin'
+        end
     end
+
+    
   end
