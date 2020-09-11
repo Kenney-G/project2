1c50ea99556c97fdecbdbdd9af479699b475b1cc
kenney-g <goadke@g.cofc.edu>
2020-09-11 19:58:32 -0400

    undo delete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200909210745) do
+ActiveRecord::Schema.define(version: 20200911235416) do
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "logs", force: :cascade do |t|
     t.string   "game_title"
