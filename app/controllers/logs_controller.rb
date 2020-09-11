c5f3927c2d7fd2ec5b258c74bf29f06dc369ae30
kenney-g <goadke@g.cofc.edu>
2020-09-11 19:41:27 -0400

    built out new log view

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -8,12 +8,19 @@ class LogsController < ApplicationController
 
   # GET: /logs/new
   get "/logs/new" do
+    @log = Log.new
     erb :"/logs/new"
   end
 
   # POST: /logs
   post "/logs" do
-    redirect "/logs"
+    @log = current_user.logs.build(title: params[:log][:title],console: params[:log][:console],desc: params[:log][:desc])
+    if @post.save
+      redirect "/posts"
+    else
+
+      erb ":logs/new"
+    end
   end
 
   # GET: /logs/5
